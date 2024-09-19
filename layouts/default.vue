e883613a56c5d9cb357d89f93ab780fd170f160b
Mohtasim Abrar Samin <samin@braincraftapps.com>
2024-09-19 12:36:09 +0600

    id updated

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <Head>
+      <Title>Sticker Maker 4.0</Title>
+      <meta name="apple-itunes-app" content="app-id=1505991796">
+    </Head>
     <slot/>
   </div>
 </template>
